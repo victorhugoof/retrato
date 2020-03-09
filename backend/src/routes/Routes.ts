7bf0ae257531dec2503e358a039293dda7eb2b2c
Victor Fernandes <victor.fernandes@triersistemas.com.br>
2020-03-09 18:39:42 -0300

    Atualizado script de start

--- backend/src/routes/Routes.ts
+++ backend/src/routes/Routes.ts
@@ -2,11 +2,13 @@ import {Router} from "express";
 import {ProdutoRoutes} from "./ProdutoRoutes";
 import {VendaRoutes} from "./VendaRoutes";
 import {ItemVendaRoutes} from "./ItemVendaRoutes";
+import {Message} from "../interfaces/Message";
 
 class Routes {
     public router = Router();
 
     constructor() {
+        this.router.get('/', (req, res) => res.send(new Message('OK')));
         ProdutoRoutes.createRoutes(this.router);
         VendaRoutes.createRoutes(this.router);
         ItemVendaRoutes.createRoutes(this.router);
